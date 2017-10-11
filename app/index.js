38ae53c5eee63117ddf2dac8b8dcd428d6f610a3
weite122 <949193531@qq.com>
2017-10-12 03:24:03 +0800

    [A]更新webpack配置

--- app/index.js
+++ app/index.js
@@ -3,6 +3,7 @@ let LoadInformation = require('./loadinformation.js')
 let LoadNews = require('./news.js')
 let Suggestions = require('./suggestion.js')
 let Tab = require('./tab.js')
+require('../css/index.scss');
 
 LoadInformation.init()
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,20 @@
-var path = require('path')
-var webpack = require('webpack')
-var ExtractTextPlugin = require("extract-text-webpack-plugin")
+const path = require('path')
+const webpack = require('webpack')
+const ExtractTextPlugin = require("extract-text-webpack-plugin")
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
 	entry: __dirname + '/app/index.js',
 	output:{
 		path: path.resolve(__dirname, 'dist'), //存放打包后文件的地方路径
 		filename: 'bundle.js'  //打包后的文件名
-	},
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+      contentBase: './dist'
+    },
 	plugins: [
+        new CleanWebpackPlugin(['dist']),
         new ExtractTextPlugin({
             filename: "[name].css",
             disable: false,
@@ -41,6 +47,30 @@ module.exports = {
                            sourceMap: true
                         }
                     }
+                })
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use:{
+                        loader:'css-loader',
+                        options: {
+                           sourceMap: true
+                        }
+                    }
+                })
+            },
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback:'style-loader',
+                    use:['css-loader',{
+                        loader:'sass-loader',
+                        options: {
+                           sourceMap: true
+                        }
+                    }]
                 })
             },
 		]
